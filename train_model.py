70d95dcaec9277e3c73e6ceb7cfb2a13e1585283
siriwxt2004 <siriwxtboonyong@gmail.com>
2026-05-22 05:30:59 +0700

    fix deployment and remove heavy dependencies

--- train_model.py
+++ train_model.py
@@ -5,39 +5,55 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 import joblib
+import os
 
+# load dataset
 df = pd.read_csv(
     "data/feedback.csv"
 )
 
+# combine text
 df["text"] = (
-    df["jd"] + " " + df["resume"]
+    df["jd"].fillna("") + " " +
+    df["resume"].fillna("")
 )
 
 X = df["text"]
 y = df["label"]
 
+# build model
 model = Pipeline([
 
     (
         "tfidf",
         TfidfVectorizer(
-            max_features=3000
+            max_features=3000,
+            stop_words="english"
         )
     ),
 
     (
         "clf",
-        LogisticRegression()
+        LogisticRegression(
+            max_iter=1000
+        )
     )
 
 ])
 
+# train
 model.fit(
     X,
     y
 )
 
+# create models folder
+os.makedirs(
+    "models",
+    exist_ok=True
+)
+
+# save model
 joblib.dump(
     model,
     "models/hr_model.pkl"
