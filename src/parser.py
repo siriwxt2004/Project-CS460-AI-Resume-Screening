7e5f936bdccecef1871785d3bf44957ce72853f4
siriwxt2004 <siriwxtboonyong@gmail.com>
2026-05-22 04:58:25 +0700

    fix parser

--- src/parser.py
+++ src/parser.py
@@ -1,19 +1,2 @@
-from PyPDF2 import PdfReader
-from docx import Document
-
-def parse_resume(file):
-    text = ""
-
-    if file.name.endswith(".pdf"):
-        pdf = PdfReader(file)
-        for page in pdf.pages:
-            extracted = page.extract_text()
-            if extracted:
-                text += extracted
-
-    elif file.name.endswith(".docx"):
-        doc = Document(file)
-        for para in doc.paragraphs:
-            text += para.text + "\n"
-
+def parse_resume(text):
     return text
